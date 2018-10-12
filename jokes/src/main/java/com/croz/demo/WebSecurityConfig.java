83818349fad1286184575b445d7d4fadd83a17d0
Mateo Srebot <mateo.srebot@gmail.com>
2018-10-12 03:18:36 +0200

    - small fixes

--- jokes/src/main/java/com/croz/demo/WebSecurityConfig.java
+++ jokes/src/main/java/com/croz/demo/WebSecurityConfig.java
@@ -17,9 +17,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/jokes").permitAll()
                 .antMatchers("/inc").permitAll()
                 .antMatchers("/dec").permitAll()
+                .antMatchers("/").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
